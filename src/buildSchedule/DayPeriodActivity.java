5783a3d00db7ea50d0e073f748f321c31b84e5ef
Mau <is716122@iteso.mx>
2020-03-28 14:30:19 -0600

    Implemented functions to create DayPeriods Lists

--- src/buildSchedule/DayPeriodActivity.java
+++ src/buildSchedule/DayPeriodActivity.java
@@ -18,4 +18,27 @@ public class DayPeriodActivity {
 		}
 		return list;
 	}
+
+
+	public static ArrayList<DayPeriodActivity> generateActivityPeriodList(){
+		ArrayList<DayPeriodActivity> list = new ArrayList<DayPeriodActivity> ();
+		return list;
+	}
+
+	public static void addActivityPeriodToList(ArrayList<DayPeriodActivity> list, Days day, int period){
+		DayPeriodActivity temp = new DayPeriodActivity(day, period);
+
+		if(list.contains(temp)) return;
+
+		list.add(temp);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof DayPeriodActivity)) return false;
+
+		DayPeriodActivity object = (DayPeriodActivity) obj;
+		return this.day.equals(object.day) && this.period==object.period;
+	}
 }
+
