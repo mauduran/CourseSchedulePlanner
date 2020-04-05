4c512df9fd44648e41fb33a195e74f27823431d9
Mau <is716122@iteso.mx>
2020-04-05 15:25:12 -0500

    Initial design for three solver methods, two of which call the one that is going to solve the CSP

--- src/buildSchedule/SchedulePlanner.java
+++ src/buildSchedule/SchedulePlanner.java
@@ -1,8 +1,31 @@
 package buildSchedule;
 
-public class SchedulePlanner {
-	
+import java.util.ArrayList;
 
-	
+public class SchedulePlanner {
+
+    public static ArrayList<Schedule> generateSchedulesFromCourses(Course ... courseList){
+        return generateSchedules(courseList);
+    }
+
+
+	public static ArrayList<Schedule> generateSchedules(Course[] courseList){
+        ArrayList<Course> courses = new ArrayList<>();
+
+        for (Course course : courseList) {
+            courses.add(course);
+        }
+        return generateSchedules(courses);
+    }
+
+
+    public static ArrayList<Schedule> generateSchedules(ArrayList<Course> courseList){
+        return new ArrayList<Schedule>();
+    }
+
+
+	public static void main(String[] args) {
+        
+    }
 	
 }
